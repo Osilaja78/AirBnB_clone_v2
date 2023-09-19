b1d6ff626274a884aa39289e03a3a4bc46533e82
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-19 12:52:19 +0100

    fixed code for task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            return {k: v for k, v in FileStorage.__objects.items()
+                    if type(v) == cls}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -52,7 +56,6 @@ class FileStorage:
     def delete(self, obj=None):
         """ Deletes obj from __objects if it's inside """
 
-        if not obj:
-            return
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        del FileStorage.__objects[key]
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
