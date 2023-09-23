db9abdaa5cacebed71df527152f4c6acd944c83a
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-23 14:37:17 +0100

    updated place.py

--- models/place.py
+++ models/place.py
@@ -8,34 +8,32 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 
 
-if models.session_type == "db":
-    relationship_table = Table('place_amenity', Base.metadata,
-                               Column('place_id', String(60),
-                                      ForeignKey('places.id'),
-                                      nullable=False),
-                               Column('amenity_id', String(60),
-                                      ForeignKey('amenities.id'),
-                                      nullable=False))
-
-
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
+    if models.session_type == "db":
+        relationship_table = Table('place_amenity', Base.metadata,
+                                Column('place_id', String(60),
+                                        ForeignKey('places.id'),
+                                        nullable=False),
+                                Column('amenity_id', String(60),
+                                        ForeignKey('amenities.id'),
+                                        nullable=False))
+        __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    reviews = relationship('Review', backref='place', cascade='delete')
-    amenities = relationship('Amenity', secondary=relationship_table,
-                             viewonly=False)
-    amenity_ids = []
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        reviews = relationship('Review', backref='place', cascade='delete')
+        amenities = relationship('Amenity', secondary=relationship_table,
+                                viewonly=False)
+        amenity_ids = []
 
     if models.session_type != 'db':
         @property
