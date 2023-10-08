33476c4606efa49bb9bfea24ac7c16aea7b5b92c
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-10-08 00:37:40 -0700

    code for taks 0 m

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # This script sets up web servers for deployment.
 
-apt-get update
+# Udate and install nginx if nt installed.
+if ! dpkg -l | grep -q nginx; then
+	apt-get update
+	apt-get -y install nginx
+fi
+
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo '<html>
@@ -11,8 +16,10 @@ echo '<html>
     Holberton School
   </body>
 </html>' > /data/web_static/releases/test/index.html
+
 # symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This  Fabric script that generates a .tgz archive
+from the contents of the web_static folder.
+"""
+
+
+from fabric.api import *
+from datetime import datetime
+
+
