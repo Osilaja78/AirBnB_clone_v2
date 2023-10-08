a1ddbbd58f91c02a6a7c103c8e8f53c93c4a2982
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-10-08 01:04:42 -0700

    code for task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,3 +9,19 @@ from fabric.api import *
 from datetime import datetime
 
 
+def do_pack():
+    """
+    Generates a .tgz archive.
+    Returns:
+        Archive path if successful
+    """
+
+    try:
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_path = f"versions/web_static_{timestamp}.tgz"
+        local("mkdir -p versions")
+        local(f"tar -cvfz {archive_path} web_static")
+        return archive_path
+    except Exception as e:
+        return f"Error: {e}"
